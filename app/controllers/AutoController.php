1bbc89a629f0f58e9b3a846c10a847fa1349bafb
Draedrans <gregoriobuenobueno@gmail.com>
2016-06-06 08:28:03 +0200

    añadidos iconos de +

--- app/controllers/AutoController.php
+++ app/controllers/AutoController.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 class AutoController extends ControllerBase
 {
 
@@ -23,7 +22,8 @@ class AutoController extends ControllerBase
         $this->view->form = new AutoForm(null, array('create' => true));
     }
 
-    public function endsWith($haystack, $needle) {
+    public function endsWith($haystack, $needle)
+    {
         // search forward starting from end minus needle length characters
         return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
     }
@@ -35,23 +35,49 @@ class AutoController extends ControllerBase
             if ($form->isValid($this->request->getPost())) {
                 if ($this->request->hasFiles()) {
                     $uploads = $this->request->getUploadedFiles();
-                    $kaboom=array();
+                    $kaboom = array();
                     foreach ($uploads as $file) {
                         $this->flash->notice("coge el archivo");
                         /* @var $file \Phalcon\Http\Request\FileInterface */
                         $fileName = $file->getName();
                         $temporaryName = $file->getTempName();
-                        $fila=1;
+                        $fila = 1;
+                        $counterino = 0;
+                        $wrongerino = 0;
                         if (($gestor = fopen($file->getTempName(), "r")) !== FALSE) {
                             $this->flash->notice("Abre el tiene archivo");
                             while (($datos = fgetcsv($gestor, 1000, ",")) !== FALSE) {
                                 $numero = count($datos);
-                                if ($fila>1) {
-                                    array_push($kaboom, array($datos[2],$datos[3],\ForceUTF8\Encoding::toUTF8($datos[0])));
+                                if ($fila > 1) {
+                                    $alumno = new Alumnos();
+                                    array_push($kaboom, array(\ForceUTF8\Encoding::toUTF8($datos[14])));
+                                    $alumno->NIE = \ForceUTF8\Encoding::toUTF8($datos[2]);
+                                    $alumno->DNI = \ForceUTF8\Encoding::toUTF8($datos[3]);
+                                    $alumno->Direccion = \ForceUTF8\Encoding::toUTF8($datos[4]);
+                                    $alumno->Localidad = \ForceUTF8\Encoding::toUTF8($datos[6]);
+                                    $alumno->Fecna = \ForceUTF8\Encoding::toUTF8(substr($datos[7], 6, 4) . "-" . substr($datos[7], 3, 2) . "-" . substr($datos[7], 0, 2));
+                                    $alumno->Provincia = \ForceUTF8\Encoding::toUTF8($datos[8]);
+                                    $alumno->Tlf = \ForceUTF8\Encoding::toUTF8($datos[9]);
+                                    $alumno->TlfUrg = \ForceUTF8\Encoding::toUTF8($datos[10]);
+                                    $alumno->Tutor = $this->request->getPost("Tutor");
+                                    $alumno->CursoActual = \ForceUTF8\Encoding::toUTF8($datos[14]);
+                                    $alumno->apellidos = \ForceUTF8\Encoding::toUTF8(trim($datos[15] . " " . $datos[16]));
+                                    $alumno->Nombre = \ForceUTF8\Encoding::toUTF8($datos[17]);
+                                    $alumno->Lugna = \ForceUTF8\Encoding::toUTF8($datos[28]);
+                                    $alumno->UltimaMatricula = \ForceUTF8\Encoding::toUTF8($datos[29]);
+                                    if ($alumno->save()) {
+                                        $counterino++;
+                                    } else {
+                                        $wrongerino++;
+                                    }
                                 }
                                 $fila++;
                             }
                             fclose($gestor);
+                            $this->flash->success("$counterino alumnos guardados");
+                            if ($wrongerino>0) {
+                                $this->flash->error("$wrongerino alumnos no guardados");
+                            }
                         }
                     }
                 }
